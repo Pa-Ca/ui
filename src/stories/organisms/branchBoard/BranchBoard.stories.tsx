6b3ef968cfdf5cc51f5c506dd8955ab46311feac
Eduardo López <32850455+eduardo98m@users.noreply.github.com>
2023-03-22 00:00:49 -0400

    Added Branch Board and Branch listing

---
+++ src/stories/organisms/branchBoard/BranchBoard.stories.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { BranchBoard } from './BranchBoard';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Design System/organisms/BranchBoard',
+  component: BranchBoard,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: { 
+    onButtonClick: {
+      table: {
+        disable: true
+      }
+    }
+  },
+} as ComponentMeta<typeof BranchBoard>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof BranchBoard> = (args: any) => {
+  return (
+    <BranchBoard {...args} />
+  );
+}
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+    width: '1300px',  
+};
