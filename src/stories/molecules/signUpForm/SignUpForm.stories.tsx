082f461509b29b22dcb5414e8fdab73c18dda56f
dedchord <dedchord@navier.local>
2023-03-27 19:24:41 -0400

    (add) SignUpForm molecule

---
+++ src/stories/molecules/signUpForm/SignUpForm.stories.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { SignUpForm } from "./SignUpForm";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Design System/Molecules/SignUpForm",
+  component: SignUpForm,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    onLogin: {
+      table: {
+        disable: true,
+      },
+    },
+    onForgotClick: {
+      table: {
+        disable: true,
+      },
+    },
+    onSignUp: {
+      table: {
+        disable: true,
+      },
+    },
+    onGoogleSignUp: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as ComponentMeta<typeof SignUpForm>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof SignUpForm> = (args: any) => {
+  return <SignUpForm {...args} />;
+};
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  color: "#EF7A08",
+  secondaryColor: "#FF8682",
+  otherLoginsColor: "#8DD3BB",
+};
