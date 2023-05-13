58efe51c03e39d08853712804b19bd4c9708a417
Amin Arriaga <aminlorenzo.14@gmail.com>
2023-05-13 16:25:45 -0400

    Delete deprecated export in index

--- src/index.tsx
+++ src/index.tsx
@@ -60,7 +60,6 @@ export * from './stories/organisms/signUpComponent/SignUpComponent';
 export * from './stories/organisms/categoryPreview/CategoryPreview';
 export * from './stories/organisms/payMethodSelector/PayMethodSelector';
 export * from './stories/organisms/branchMainSummary/BranchMainSummary';
-export * from './stories/organisms/createReservationModal/CreateReservationModal';
 export * from './stories/organisms/resetPasswordComponent/ResetPasswordComponent';
 export * from './stories/organisms/forgotPasswordComponent/ForgotPasswordComponent';
 export * from './stories/organisms/recoverPasswordComponent/RecoverPasswordComponent';
