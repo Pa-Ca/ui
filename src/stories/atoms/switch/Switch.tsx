62804653ab20b8810926b31039b4a3861899db16
Amin Arriaga <aminlorenzo.14@gmail.com>
2023-06-08 16:36:44 -0400

    Add switch atom

---
+++ src/stories/atoms/switch/Switch.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import classnames from "classnames";
+import styles from "./switch.module.scss";
+import styleVariables from "../../assets/scss/variables.module.scss";
+
+interface SwitchProps {
+  /**
+   * Switch state
+   */
+  active: boolean;
+  /**
+   * On click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Switch = ({
+  active,
+  onClick,
+  ...props
+}: SwitchProps): JSX.Element => {
+  return (
+    <div
+      className={classnames(
+        styles["switch--container"],
+        active
+          ? styles["switch--container-active"]
+          : styles["switch--container-inactive"]
+      )}
+      onClick={onClick}
+    >
+      <div
+        className={classnames(
+          styles["switch--circle"],
+          active
+            ? styles["switch--circle-active"]
+            : styles["switch--circle-inactive"]
+        )}
+        style={{ left: active ? "55%" : "5%" }}
+      />
+    </div>
+  );
+};
