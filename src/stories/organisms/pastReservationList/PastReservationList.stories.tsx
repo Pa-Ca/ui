0405fce35c09fa6807ce3f4d9b0c17e10bcb66c5
Carlos Sivira <carlos.csivira@gmail.com>
2023-07-31 13:46:00 -0400

    fix: formatAMPM string type

--- src/stories/organisms/pastReservationList/PastReservationList.stories.tsx
+++ src/stories/organisms/pastReservationList/PastReservationList.stories.tsx
@@ -28,13 +28,13 @@ const reservationsStatus: ReservationStatusObject[] = [
 ]
 
 function formatAMPM(date: Date) {
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'pm' : 'am';
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let ampm = hours >= 12 ? 'pm' : 'am';
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + ampm;
+  let strMinutes = minutes < 10 ? '0'+minutes : minutes;
+  let strTime = hours + ':' + strMinutes + ' ' + ampm;
   return strTime;
 }
 
