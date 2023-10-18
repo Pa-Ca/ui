fc23da394823f5fb5cd9968d4e12d7ace6bf641f
Amin Arriaga <aminlorenzo.14@gmail.com>
2023-10-18 09:15:59 -0400

    Create promotion banner molecule

--- src/stories/molecules/promotionBanner/PromotionBanner.tsx
+++ src/stories/molecules/promotionBanner/PromotionBanner.tsx
@@ -18,6 +18,10 @@ export interface PromotionBannerProps {
    */
   date: Date;
   /**
+   * On select function
+   */
+  onSelect?: () => void;
+  /**
    * Width
    */
   width?: string;
@@ -34,30 +38,39 @@ export const PromotionBanner = ({
   price,
   description,
   date,
+  onSelect,
   width,
   height,
   ...props
 }: PromotionBannerProps) => {
   return (
     <Box className={styles["promotion-banner--container"]} style={{ width, height }}>
-      <Box>
-        <Text type="h4" weight="700">
-          {price}
-        </Text>
-        <Text type="h6" className={styles["promotion-banner--text"]} weight="600">
-          {description}
+      <Box style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+        <Text type="h4">Promociones</Text>
+        <Text type="h6" weight="400">
+          Términos y condiciones aplican
         </Text>
       </Box>
-
-      <Box style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-        <Text type="h6" className={styles["promotion-banner--text"]}>
-          Hasta el {date.toLocaleDateString("es-ES", { day: "numeric", month: "long" })}
-        </Text>
-
+      <Box className={styles["promotion-banner--data-container"]}>
         <Box>
-          <Button primary size="small">
-            <Text primaryButtonStyle>Seleccionar</Text>
-          </Button>
+          <Text type="h4" weight="700">
+            {price}
+          </Text>
+          <Text type="h6" className={styles["promotion-banner--text"]} weight="600">
+            {description}
+          </Text>
+        </Box>
+
+        <Box style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+          <Text type="h6" className={styles["promotion-banner--text"]}>
+            Hasta el {date.toLocaleDateString("es-ES", { day: "numeric", month: "long" })}
+          </Text>
+
+          <Box>
+            <Button primary size="small" onClick={onSelect}>
+              <Text primaryButtonStyle>Seleccionar</Text>
+            </Button>
+          </Box>
         </Box>
       </Box>
     </Box>
